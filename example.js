9648abfeb7ecc5f4b45536fc88fc816c24116bfb
tx19980520 <ty0207@sjtu.edu.cn>
2017-12-17 23:52:18 +0800

    review feature
    review feature

---
+++ example.js
@@ -0,0 +1,103 @@
+import React, { Component }from 'react';
+export class ReviewModel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleViewClick = this.handleViewClick.bind(this);
+    this.handleNotviewClick = this.handleNotviewClick.bind(this);
+    this.state = {isView: false};
+  }
+  handleViewClick() {
+    this.setState({isView: true});
+  }
+
+  handleNotviewClick() {
+    this.setState({isView: false});
+  }
+
+  render() {
+    const isView = this.state.isView;
+
+    let button = null;
+    if (isView) {
+      button = <NotReviewButton onClick={this.handleNotviewClick} />;
+    } else {
+      button = <ReViewButton onClick={this.handleViewClick} />;
+    }
+
+    return (
+      <div>
+        <Views isView={isView} />
+        {button}
+      </div>
+    );
+  }
+}
+
+function UserView(props) {
+  return <textarea>Welcome back!</textarea>;
+}
+
+function UserNotView(props) {
+  return <div></div>;
+}
+
+function Views(props) {
+  const isView = props.isView;
+  if (isView) {
+    return <UserView />;
+  }
+  return <UserNotView />;
+}
+
+function ReViewButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      评论
+    </button>
+  );
+}
+
+function NotReviewButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      取消评论
+    </button>
+  );
+}
+
+
+
+export default class Catalog extends Component {
+    render() {
+        let data = [
+            {"Name":111,"Price":2.5},
+            {"Name":222,"Price":3.5},
+        ];
+        let list;
+        list = data.map(function (item, index) {
+                return (
+                  <div>
+                    <Example key={index} name={item.Name} price={item.Price}/>
+                    <ReviewModel />
+                  </div>
+                )
+            });
+
+        return (
+            list
+        );
+    }
+
+}
+
+export  class Example extends Component {
+    render() {
+        return (
+            <div>
+                <p>Name:{ this.props.name }</p>
+                <p>Price:{ this.props.price }</p>
+                <p>Comment:{ this.props.comm }</p>
+            </div>
+        )
+    }
+}
--- where_to_eat/src/example.js
+++ where_to_eat/src/example.js
@@ -1,12 +1,85 @@
 import React, { Component }from 'react';
+export class CommentInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmitClick = this.handleSubmitClick.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+    this.changeComment = this.changeComment.bind(this);
+    this.state = {isView:this.props.isView, username:"Tony",nowComment:""};
+  }
+  changeComment(event) {
+    this.setState({nowComment: event.target.value});
+  }
+  changeUser(event) {
+  this.setState({username: event.target.value});
+}
+  handleSubmitClick() {
+    if (this.props.onSubmit) {
+        this.setState({isView: false});
+        const {username, nowComment } = this.state
+        this.props.onSubmit({username, nowComment})
+        }
+      this.setState({nowComment:""});
+  }
+  render() {
+    let submit = <div></div>;
+    let username = <div></div>;
+    submit = <SubmitButton onClick={this.handleSubmitClick} />;
+    username = <UserName username={this.state.username} onChange={this.changeUser}/>;
+    return (
+      <div>
+      {username}
+        <Views isView={this.props.isView} onChange={this.changeComment}/>
+        {submit}
+      </div>
+    );
+  }
+}
+export class Commentline extends React.Component {
+  static defaultProps = {
+    comment:[]
+  }
+  render(){
+    return(
+      <div>
+      <span>{this.props.comment.username} </span>
+        <p>{this.props.comment.nowComment}</p>
+      </div>
+    );
+  }
+}
+export class CommentList extends React.Component {
+  static defaultProps = {
+    comments: []
+  }
+  render() {
+  return (
+     <div>
+       {this.props.comments.map((comment, i) =>
+         <Commentline comment={comment} key={i} />
+       )}
+     </div>
+   );
+ }
+}
 export class ReviewModel extends React.Component {
   constructor(props) {
     super(props);
     this.handleViewClick = this.handleViewClick.bind(this);
     this.handleNotviewClick = this.handleNotviewClick.bind(this);
-    this.state = {isView: false};
+    this.handleSubmitComment= this.handleSubmitComment.bind(this);
+    this.state = {isView: false ,comments:[]};
   }
-
+  handleSubmitComment (comment) {
+    if (!comment.username) return alert('请输入用户名');
+    if (!comment.nowComment) return alert('请输入评论内容');
+    this.state.comments.push(comment);
+    this.setState({
+      comments: this.state.comments
+    });
+    this.setState({isView: false});
+    alert(this.state.comments[0].nowComment)
+  }
   handleViewClick() {
     this.setState({isView: true});
   }
@@ -17,25 +90,29 @@ export class ReviewModel extends React.Component {
 
   render() {
     const isView = this.state.isView;
-
+    let previous = <div>noi</div>
     let button = null;
+    let review = <div></div>;
     if (isView) {
       button = <NotReviewButton onClick={this.handleNotviewClick} />;
+      review = <CommentInput isView={this.state.isView} onSubmit={this.handleSubmitComment}/>
+      previous = <CommentList comments={this.state.comments} />;
     } else {
       button = <ReViewButton onClick={this.handleViewClick} />;
+      previous = <CommentList comments={this.state.comments} />;
     }
-
     return (
       <div>
-        <Views isView={isView} />
         {button}
+        {review}
+        {previous}
       </div>
     );
   }
 }
 
 function UserView(props) {
-  return <textarea>Welcome back!</textarea>;
+  return (<div><font>您的评论</font><textarea onChange={props.onChange}></textarea></div>);
 }
 
 function UserNotView(props) {
@@ -45,11 +122,19 @@ function UserNotView(props) {
 function Views(props) {
   const isView = props.isView;
   if (isView) {
-    return <UserView />;
+    return <UserView onChange={props.onChange}/>;
   }
   return <UserNotView />;
 }
-
+function UserName(props) {
+  return(<div><font>用户名</font>
+    <input type="text" value={props.username} onChange={props.onChange}/> </div>);
+}
+function SubmitButton(props) {
+    return (<button onClick={props.onClick}>
+    提交
+    </button>);
+}
 function ReViewButton(props) {
   return (
     <button onClick={props.onClick}>
